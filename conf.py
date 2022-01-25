0f0dda0b0e90304a6fb3f12ba417094020fa557f
Willem Essenstam <31310808+wessenstam@users.noreply.github.com>
2022-01-25 10:36:06 +0100

    Automation using Drone

--- conf.py
+++ conf.py
@@ -52,8 +52,8 @@ source_suffix = '.rst'
 master_doc = 'index'
 
 # General information about the project.
-project = u'ThycoticCentrify Enablement'
-copyright = u'2021 ThycoticCentrify'
+project = u'Test Drive'
+copyright = u'2022 ThycoticCentrify'
 author = u'ThycoticCentrify Technical Enablement'
 
 # The version info for the project you're documenting, acts as replacement for
